e18a14120b6891fb39b5ba0f49065b699f052511
phil-schmidtke <mail@phil-schmidtke.de>
2023-05-23 15:47:36 +0200

    core: fix get Balance wrapper

--- packages/core/lib/web3/getBalance.tsx
+++ packages/core/lib/web3/getBalance.tsx
@@ -1,10 +1,10 @@
 import Web3 from "web3"
-import { getNetworkByChainId } from "../helpers/networks"
+import { getRpcByChainId } from "../helpers/networks"
 
 type ChainId = "0x1" | "0x38" | "0x89" | "0xa4b1"
 
 export function getBalance(wallet: string, chainId: string | ChainId) {
-    const rpc = getNetworkByChainId(chainId)
+    const rpc = getRpcByChainId(chainId)
     if (rpc && rpc.length > 2) {
         const web3 = new Web3(rpc)
         return web3.eth.getBalance(wallet)
