903d04517e3238f338712df5024752b8e746d9cb
phil-schmidtke <mail@phil-schmidtke.de>
2023-05-23 14:41:00 +0200

    core: add missing return type

--- packages/core/lib/helpers/conversions.ts
+++ packages/core/lib/helpers/conversions.ts
@@ -1,7 +1,7 @@
-export function weiToEther(wei: number) {
+export function weiToEther(wei: number): number {
     return wei * 10 ** -18;
 }
 
-export function gweiToEther(gwei: number) {
+export function gweiToEther(gwei: number): number {
     return gwei * 10 ** -9;
 }
