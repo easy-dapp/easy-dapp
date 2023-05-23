a8d8e1d4357f9287c541f85a4373a25b77fb83e3
phil-schmidtke <mail@phil-schmidtke.de>
2023-05-23 14:42:08 +0200

    core: add weiToGwei

--- packages/core/lib/helpers/conversions.ts
+++ packages/core/lib/helpers/conversions.ts
@@ -5,3 +5,7 @@ export function weiToEther(wei: number): number {
 export function gweiToEther(gwei: number): number {
     return gwei * 10 ** -9;
 }
+
+export function weiToGwei(wei: number) {
+    return wei * 10 ** -9;
+}
